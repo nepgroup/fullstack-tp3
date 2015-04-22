da5aaa4337c528d8a5c987638f20f5b1b9c42847
Nicolas Pessolani <info@nepgroup.com.ar>
2015-04-22 18:36:44 -0300

    Buscar con indexOf

--- buscar_vocales.js
+++ buscar_vocales.js
@@ -1,3 +1,5 @@
+
+// Vowels withOut indexOf
 var arr = ['follow', 'the', 'white', 'rabbit'];
 var vocales = [];
 
@@ -5,6 +7,24 @@ for (var i = 0; i < arr.length; i++) {
 	var sum = 0;
 	for (var j = 0; j < arr[i].length; j++) {		
 		if ( arr[i].charAt(j).match(/[aeiouAEIOU]/) ){
+			sum++;
+		};
+	};
+	vocales.push(sum);
+};
+
+console.log(arr);
+console.log(vocales);
+
+// Vowels with indexOf
+
+var arr = ['follow', 'the', 'white', 'rabbit'];
+var vocales = [];
+
+for (var i = 0; i < arr.length; i++) {
+	var sum = 0;
+	for (var j = 0; j < arr[i].length; j++) {		
+		if (["a", "e", "i", "o", "u"].indexOf(arr[i][j]) !== -1){
 			sum++;
 		};
 	};
